d5c16b95136aec3a2f6970fb949e92c240122c65
pdonlon <pdonlon57@gmail.com>
2014-10-28 21:38:49 -0400

    icons on front page now fade in

--- style2.js
+++ style2.js
@@ -1,9 +1,13 @@
 var p = 0;
 var speed = 200;
+var moving = true;
 
 function moveit() {
     p += 0.05;
-
+    
+    if(!moving)
+        return;
+    
     var r = 300;
     var xcenter = document.getElementById("logo").getBoundingClientRect().left;
     var ycenter = document.getElementById("logo").getBoundingClientRect().top*2;
@@ -28,23 +32,42 @@ function moveit() {
         top: newTop1,
         left: newLeft1,
     },speed, function() {
-        moveit();
     });
     
     $('#friends3').animate({
                            top: newTop2,
                            left: newLeft2,
                            },speed, function() {
-                           moveit();
                            });
     
     $('#friends4').animate({
                            top: newTop3,
                            left: newLeft3,
                            },speed, function() {
-                           moveit();
                            });
  }
+
+function startit() {
+    moving = true;
+    moveit();
+    console.log("HEY");
+}
+
+function stopit() {
+    moving = false;
+    $('.friends img').stop(true, false);
+}
+
+$('.friends img').hover(function() {
+    stopit();
+});
+
+$('.friends img').on("mouseout", function() {
+                    startit();
+                    });
+
+
 $(document).ready(function() {
-    moveit();    
+                  $('#frienddiv').delay(800).animate({opacity : 1},  600 );
+    moveit();
 });
